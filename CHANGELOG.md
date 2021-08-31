e0ddeabe2795e56793cd351e374574ca0b05ada6
tykan <kang3603a@gmail.com>
2021-08-31 11:54:49 +0900

    0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 0.1.1
+* assert when the widget is not found.
+
 ## 0.1.0
 * initial publish
--- lib/src/finder.dart
+++ lib/src/finder.dart
@@ -1,29 +1,54 @@
 import 'package:flutter/material.dart';
 
 class WidgetFinder {
+  /// Use GlobalKey to find widget.
+  ///
+  /// Declare it as global constant or Manage it inside other state-management(provider, riverpod...)
   final GlobalKey key;
 
-  WidgetFinder.of(this.key);
+  WidgetFinder.of(this.key)
+      : assert(key.currentContext?.findRenderObject() != null,
+            'No widget in the tree that matches this global key.');
 
-  // 해당 위젯의 context
-  BuildContext? get context => key.currentContext;
+  /// Context of the widget.
+  BuildContext get context => key.currentContext!;
 
-  RenderBox get box => context?.findRenderObject() as RenderBox;
+  /// RenderBox of the widget.
+  RenderBox get box => context.findRenderObject() as RenderBox;
 
+  /// Size of the widget.
   Size get size => box.size;
 
+  /// Size.width of the widget.
   double get width => size.width;
+
+  /// Size.height of the widget.
   double get height => size.height;
 
+  /// TopLeft point of the widget.
   Offset get topLeft => box.localToGlobal(Offset.zero);
+
+  /// TopCenter point of the widget.
   Offset get topCenter => topLeft + Offset(width / 2, 0);
+
+  /// TopRight point of the widget.
   Offset get topRight => topLeft + Offset(width, 0);
 
+  /// CenterLeft point of the widget.
   Offset get centerLeft => topLeft + Offset(0, height / 2);
+
+  /// Center point of the widget.
   Offset get center => topLeft + Offset(width / 2, height / 2);
+
+  /// CenterRight point of the widget.
   Offset get centerRight => topLeft + Offset(width, height / 2);
 
+  /// BottomLeft point of the widget.
   Offset get bottomLeft => topLeft + Offset(0, height);
+
+  /// BottomCenter point of the widget.
   Offset get bottomCenter => topLeft + Offset(width / 2, height);
+
+  /// BottomRight point of the widget.
   Offset get bottomRight => topLeft + Offset(width, height);
 }
